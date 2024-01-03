00382935ed90341d474d41cd4f1d21610fc2c8d2
superman2080 <superman2080@naver.com>
2024-01-03 13:51:04 +0900

    logic developing...

---
+++ Assets/02. Scripts/Entities/Playable/TempPlayable.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempPlayable : PlayableCtrl
+{
+
+    protected override void OnEntityDied()
+    {
+
+    }
+}
