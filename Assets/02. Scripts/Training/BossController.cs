62bce5eb72109f939c086872acb78bca332ffad1
AI-Expert-04 <hwakkillhan@gmail.com>
2024-01-05 14:14:40 +0900

    .

--- Assets/02. Scripts/Training/BossController.cs
+++ Assets/02. Scripts/Training/BossController.cs
@@ -4,251 +4,139 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
-    // public Rigidbody rb;
-    // public AgentController ac;
-    // public Renderer squareRenderer; // 정사각형 오브젝트의 렌더러
-    // private Vector3 initialPosition; // 보스 캐릭터의 초기 위치를 저장할 변수
+    public Rigidbody rb;
+    public BossSpawner bs;
+    public AgentController ac;
+    public Renderer squareRenderer; // 정사각형 오브젝트의 렌더러
+    private Vector3 initialPosition; // 보스 캐릭터의 초기 위치를 저장할 변수
 
-    // public float moveSpeed = 5f;
+    public float moveSpeed = 5f;
 
-    // public bool reset = false; // 리셋 비활성화
-    // public bool stop = false; // 멈추기 비활성화
-    // public bool fx = false; // fx = 비활성화
+    public bool reset = false; // 리셋 비활성화
+    public bool stop = false; // 멈추기 비활성화
+    public bool fx = false; // fx = 비활성화
 
-    // // 월드 좌표  0, 0, 0을 기// 랜덤.범위 -9 ~ 9에서  보스가 100개 정도 생성되었으면 좋겠어
-    // /*
-    //                            (1)
-    //                             -  (2)
-    //                         -       -  (3)
-    //                     -               -  (4)
-    //                 _                       -  ...
-    //             _                               -
-    //                 _                       -
-    //                     -               -
-    //                         -       -
-    //                             - 
+    void Start()
+    {
+        ac = GetComponent<AgentController>(); // 에이전트 구성요소 받기
+        rb = GetComponent<Rigidbody>(); // 물리엔진 구성요소 받기
+        bs = GetComponent<BossSpawner>(); // 에이전트 구성요소 받기
 
-    // */
-   
+        // 시작 시 초기 위치를 저장
+        initialPosition = transform.position;
+    }
 
-    // void Start()
-    // {
-    //     ac = GetComponent<AgentController>(); // 에이전트 구성요소 받기
-    //     rb = GetComponent<Rigidbody>(); // 물리엔진 구성요소 받기
+    public void ResetBoss()
+    {
+        reset = true;
+    }
 
-    //     // 시작 시 초기 위치를 저장
-    //     initialPosition = transform.position;
-    // }
+    void UpdateSquareColor(Color color)
+    {
+        squareRenderer.material.color = color;
+    }
 
-    // // 리셋보스 -> 나중에 에이전트 컨트롤러에서 씀
-    // public void ResetBoss()
-    // {
-    //     transform.position = initialPosition;
-    //     reset = true;  
-    // }
+    public void MoveBoss(float moveAction)
+    {
+        // 이동 코드
+        Vector3 moveDirection = new Vector3(0f, 0f, moveAction);
+        rb.velocity = moveDirection * moveSpeed;
+    }
 
-    // void UpdateSquareColor(Color color)
-    // {
-    //     // 정사각형 오브젝트의 색깔 변경
-    //     squareRenderer.material.color = color;
-    // }
+    private void FixedUpdate()
+    {
+        // 만약 한 에피소드가 끝나면
+        if (ac.episodeFinished)
+        {
+            // 나중에 추가 기능 할거임ㅋ
+            UpdateSquareColor(Color.white);
+            return;
+        }
 
-    // public void MoveBoss(float moveAction)
-    // {
-    //     // 이동 코드
-    //     Vector3 moveDirection = new Vector3(0f, 0f, moveAction);
-    //     rb.velocity = moveDirection * moveSpeed;
-    // }
+        if (reset)
+        {
+            reset = false;
+            stop = false;
+            return; 
+        }
+        float distanceToCenter = Vector3.Distance(Vector3.zero, transform.position);
 
-    // private void FixedUpdate()
-    // {
-    //     // 만약 한 에피소드가 끝나면
-    //     if (bc.episodeFinished)
-    //     {
-    //         // 나중에 추가 기능 할거임ㅋ
-    //         UpdateSquareColor(Color.pink); // 시간 초과 시 빨간색으로 변경
-    //         return;
-    //     }
+        float distanceToInitialPosition = Vector3.Distance(transform.position, initialPosition);   // 보스가 초기 위치로 되돌아가는 로직 추가
 
-    //     if (reset)
-    //     {
-    //         // 랜덤.범위 -9 ~ 9
-    //         float positionTheta = Random.Range(-Mathf.PI, Mathf.PI);
-    //         float rotationTheta = Random.Range(-Mathf.PI, Mathf.PI);
+        // 플레이어와 가까워졌을 때 true 반환
+        Collider[] colliders2 = Physics.OverlapSphere(transform.position, 5.0f, LayerMask.GetMask("PLAYER"));
 
-    //         reset = false; // 리셋 비활성화
-    //         stop = false; // stop 비활성화
-
-    //         landingZoneNormal.SetActive(true);  // 기본 목표물 활성화
-    //         landingZoneSuccess.SetActive(false); // 성공 목표물 비활성화
-    //         landingZoneFail.SetActive(false); // 실패 목표물 비활성화
-    //         return; 
-    //     }
-
-      
-    //     float distanceToInitialPosition = Vector3.Distance(transform.position, initialPosition);   // 보스가 초기 위치로 되돌아가는 로직 추가
-    //     bool collidedWithPlayer = CheckCollisionWithPlayer();  // 플레이어와의 충돌 여부 확인
-    //     bool timeElapsed = CheckTimeElapsed();  // 10초 안에 플레이어와 충돌하지 않았을 경우 음수 보상 부여
-    //     bool outOfCircle = CheckOutOfCircle(); // 원형 밖으로 나갔을 경우 음수 보상 부여
-    //     bool playerProximity = CheckPlayerProximity(); // 플레이어와 가까워졌을 때 10%의 보상 부여
-    //     bool playerDistanceIncreased = CheckPlayerDistanceIncreased();  // 플레이어와 멀어졌을 때 -20% 음수 보상 부여
-
-    //     if (collidedWithPlayer)
-    //     {
-    //         // 보상 최대점 부여
-    //         ac.AddReward(1f);
-    //         UpdateSquareColor(Color.blue); // 플레이어와 충돌 시 파란색으로 변경
-    //         return true;
-    //     }
+        // 플레이어와 멀어졌을 때 true 반환
+        Collider[] colliders3 = Physics.OverlapSphere(transform.position, 10.0f, LayerMask.GetMask("PLAYER"));
         
-    //     if (timeElapsed)
-    //     {
-    //         // 40%의 음수 보상 부여
-    //         ac.AddReward(-0.4f);
-    //         UpdateSquareColor(Color.red); // 시간 초과 시 빨간색으로 변경
-    //         return true;
-    //     }
+        if (Time.timeSinceLevelLoad > 10f)
+        {
+            // 40%의 음수 보상 부여
+            ac.AddReward(-0.4f);
+            UpdateSquareColor(Color.red); // 시간 초과 시 빨간색으로 변경
+            ac.EndEpisode(0);
+        }
 
-    //     if (outOfCircle)
-    //     {
-    //         // 음수 보상 최하점 부여
-    //         ac.AddReward(-1f);
-    //         UpdateSquareColor(Color.red); // 원 밖으로 나갔을 때 빨간색으로 변경
-    //         return true;
-    //     }
-
+        if (distanceToCenter > 20f)
+        {
+            UpdateSquareColor(Color.red); // 원 밖으로 나갔을 때 빨간색으로 변경
+            print("원 밖으로 나감 ㅋ");
+            // 원의 둘레를 벗어날 때 가장 안 좋은 음수 보상을 부여
+            ac.AddReward(-1.5f);
+            ac.EndEpisode(-1); // 에피소드 종료, 보상은 -1로 설정
+        }
        
-    //     if (playerProximity)
-    //     {
-    //         ac.AddReward(0.1f);
-    //         UpdateSquareColor(Color.green); // 플레이어와 가까워질 때 초록색으로 변경
-    //     }
+        if (colliders2.Length > 0)
+        {
+            ac.AddReward(0.1f);
+            UpdateSquareColor(Color.green); // 플레이어와 가까워질 때 초록색으로 변경
+        }
 
         
-    //     if (playerDistanceIncreased)
-    //     {
-    //         ac.AddReward(-0.2f);
-    //         UpdateSquareColor(Color.yellow); // 플레이어와 멀어질 때 노란색으로 변경
-    //     }
-    //     // 여기에 원하는 로직을 추가
-    //     float moveAction = Input.GetAxis("Horizontal"); // 가로 방향의 입력을 받음
-    //     MoveBoss(moveAction); // 보스 캐릭터 이동
-    // }
+        if (colliders3.Length == 0)
+        {
+            ac.AddReward(-0.2f);
+            UpdateSquareColor(Color.yellow); // 플레이어와 멀어질 때 노란색으로 변경
+        }
 
-    // void OnCollisionEnter(Collision collision)
-    // {
-    //     if (collision.gameObject.CompareTag("Player"))
-    //     {
-    //         // 플레이어와 충돌한 경우 양수 보상 부여
-    //         float reward = 1f;
-    //         ac.AddReward(reward);
+        if (stop)
+        {
+            print("스탑");
+            UpdateSquareColor(Color.white);
+            return;
+        }
+    }
 
-    //         // 충돌 시 에피소드 종료 여부 확인
-    //         bool shouldEndEpisode = CheckEpisodeCompletion();
-    //         if (shouldEndEpisode)
-    //         {
-    //             // 종료 시 추가 보상 또는 패널티 부여 (예: 에피소드 종료 보상 또는 음수 보상)
-    //             float episodeEndReward = 10f;
-    //             ac.AddReward(episodeEndReward);
-    //             // 에피소드 종료
-    //             ac.EndEpisode();
-    //         }
-    //     }
-    // }
+    void Update()
+    {
+        // float moveAction = 1.0f; // 모든 보스가 같은 이동 입력을 받도록 수정
+        float moveAction = 0f;
+        MoveBoss(moveAction); // 보스 캐릭터 이동
+    }
 
-    // bool CheckEpisodeCompletion()
-    // {
-    //     Debug.Log("CheckTimeElapsed called");
-    //     // 보스가 초기 위치로 되돌아가는 로직 추가
-    //     float distanceToInitialPosition = Vector3.Distance(transform.position, initialPosition);
+    void OnCollisionEnter(Collision collision)
+    {
 
-    //     // 플레이어와의 충돌 여부 확인
-    //     bool collidedWithPlayer = CheckCollisionWithPlayer();
+        // 에피소드가 끝나거나 멈추면 리턴
+        if (ac.episodeFinished || stop)
+        {
+            return;
+        }
 
-    //     // 10초 안에 플레이어와 충돌하지 않았을 경우 음수 보상 부여
-    //     bool timeElapsed = CheckTimeElapsed();
+        if (collision.gameObject.CompareTag("PLAYER"))
+        {
+            print("맞음");
+            UpdateSquareColor(Color.blue);
+            ac.AddReward(1f);
+            ac.EndEpisode(1000);
+        }
 
-    //     // 원형 밖으로 나갔을 경우 음수 보상 부여
-    //     bool outOfCircle = CheckOutOfCircle();
-
-    //     // 종료 조건을 만족하는 경우 에피소드 종료
-    //     if (collidedWithPlayer)
-    //     {
-    //         // 보상 최대점 부여
-    //         ac.AddReward(1f);
-    //         UpdateSquareColor(Color.blue); // 플레이어와 충돌 시 파란색으로 변경
-    //         return true;
-    //     }
-        
-    //     if (timeElapsed)
-    //     {
-    //         // 40%의 음수 보상 부여
-    //         ac.AddReward(-0.4f);
-    //         UpdateSquareColor(Color.red); // 시간 초과 시 빨간색으로 변경
-    //         return true;
-    //     }
-
-    //     if (outOfCircle)
-    //     {
-    //         // 음수 보상 최하점 부여
-    //         ac.AddReward(-1f);
-    //         UpdateSquareColor(Color.red); // 원 밖으로 나갔을 때 빨간색으로 변경
-    //         return true;
-    //     }
-
-    //     // 플레이어와 가까워졌을 때 10%의 보상 부여
-    //     bool playerProximity = CheckPlayerProximity();
-    //     if (playerProximity)
-    //     {
-    //         ac.AddReward(0.1f);
-    //         UpdateSquareColor(Color.green); // 플레이어와 가까워질 때 초록색으로 변경
-    //     }
-
-    //     // 플레이어와 멀어졌을 때 -20% 음수 보상 부여
-    //     bool playerDistanceIncreased = CheckPlayerDistanceIncreased();
-    //     if (playerDistanceIncreased)
-    //     {
-    //         ac.AddReward(-0.2f);
-    //         UpdateSquareColor(Color.yellow); // 플레이어와 멀어질 때 노란색으로 변경
-    //     }
-
-    //     return false;
-    // }
-
-    // bool CheckCollisionWithPlayer()
-    // {
-    //     Collider[] colliders = Physics.OverlapSphere(transform.position, 1.0f, LayerMask.GetMask("Player"));
-    //     return colliders.Length > 0;
-    // }
-
-    // bool CheckTimeElapsed()
-    // {
-    //     // 10초 안에 충돌하지 않았을 경우 true 반환
-    //     bool timeElapsed = Time.timeSinceLevelLoad > 10f;
-
-    //     return timeElapsed;
-    // }
-
-    // bool CheckOutOfCircle()
-    // {
-    //     // 임의의 원 밖으로 나갔을 경우 true 반환
-    //     Vector3 centerOfCircle = new Vector3(0, 0, 0);
-    //     float circleRadius = 10f;
-    //     float distanceToCenter = Vector3.Distance(centerOfCircle, transform.position);
-    //     return distanceToCenter > circleRadius;
-    // }
-
-    // bool CheckPlayerProximity()
-    // {
-    //     // 플레이어와 가까워졌을 때 true 반환
-    //     Collider[] colliders = Physics.OverlapSphere(transform.position, 5.0f, LayerMask.GetMask("Player"));
-    //     return colliders.Length > 0;
-    // }
-
-    // bool CheckPlayerDistanceIncreased()
-    // {
-    //     // 플레이어와 멀어졌을 때 true 반환
-    //     Collider[] colliders = Physics.OverlapSphere(transform.position, 10.0f, LayerMask.GetMask("Player"));
-    //     return colliders.Length == 0;
-    // }
+        else
+        {
+            stop = true;   
+            UpdateSquareColor(Color.red);
+            ac.AddReward(-1f);
+            ac.EndEpisode(-1);
+        }
+    }
 }
