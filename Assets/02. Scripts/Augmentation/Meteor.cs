9606dd322d601ba013b595c7f48a0bbeef1618f6
Fsahn <haider1315s@gmail.com>
2024-01-08 18:23:10 +0900

    Nuclear Bomb & Meteor

--- Assets/02. Scripts/Augmentation/Meteor.cs
+++ Assets/02. Scripts/Augmentation/Meteor.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 
 public class Meteor : Augmentation
 {
+    public float meteorDmg = 10f;
     public float splashDamageRadius = 5f; // 스플래쉬 피해 반경
 	public GameObject meteorPrefab; // 메테오 프리팹
     private Collider[] enemies; // 모든 적을 저장할 배열
 
     public Meteor(PlayableCtrl player, int level, AugmentationEventType eventType) : base(player, level, eventType)
 	{
-
+        
 	}
 
 	public override void AugmentationEffect(Entity sender, EventArgs e)
@@ -37,7 +38,7 @@ public class Meteor : Augmentation
                     int randomEnemyIdx = UnityEngine.Random.Range(0, enemies.Length);
                     if (Util.IsTargetInSight(Camera.main.transform, enemies[randomEnemyIdx].transform, Camera.main.fieldOfView))
                     {
-                        LaunchMeteorAttack(enemies[randomEnemyIdx].transform.position);
+                        LaunchMeteorAttack(enemies[randomEnemyIdx].transform.position, 3f);
                         break;
                     }
                     else
@@ -47,28 +48,30 @@ public class Meteor : Augmentation
         }
     }
 
-    private IEnumerator delay()
-    {
-        yield return new WaitForSeconds(2f);
-    }
-
-    void LaunchMeteorAttack(Vector3 targetPosition)
+    private IEnumerator LaunchMeteorAttack(Vector3 targetPosition, float time)
     {
 
         // 메테오 프리팹을 생성하여 타겟 위치에 떨어뜨림
-        GameObject meteor = UnityEngine.Object.Instantiate(meteorPrefab, targetPosition, Quaternion.identity);
+        GameObject meteor = UnityEngine.Object.Instantiate(meteorPrefab, targetPosition + Vector3.up * 50, Quaternion.identity);
 
-        
+        float dT = 0;
+        Vector3 origin = targetPosition + Vector3.up * 50;
+        while(dT < time)
+		{
+            meteor.transform.position = Vector3.Lerp(origin, targetPosition, dT / time);
+            yield return null;
+            dT += Time.deltaTime;
+		}
 
-        // 스플래쉬 피해 반경 내의 적에게 피해를 줌
-        Collider[] colliders = Physics.OverlapSphere(targetPosition, splashDamageRadius); 
+        Collider[] col = Physics.OverlapSphere(targetPosition, splashDamageRadius, 1 << LayerMask.NameToLayer("ENEMY"));
 
-        foreach (var enemy in UnityEngine.Object.FindObjectsOfType<EnemyCtrl>())
-        {
-            enemy.TakeDamage(enemy.hp);
-        }
 
-        // 2초 후에 메테오 삭제
-        UnityEngine.Object.Destroy(meteor, 2f);
+        if(col.Length > 0)
+		{
+		    foreach (var enemy in col)
+		    {
+                enemy.GetComponent<Entity>().TakeDamage(player, meteorDmg);
+		    }
+		}
     }
 }
--- Assets/02. Scripts/Augmentation/NuclearBomb.cs
+++ Assets/02. Scripts/Augmentation/NuclearBomb.cs
@@ -5,9 +5,41 @@ using UnityEngine;
 
 public class NuclearBomb : Augmentation
 {
+	public float skillTime = 240f;
+	public int lvl = 1;
+	private int currentLevel = 0;
+	private IEnumerator enumerator;
+
 	public NuclearBomb(PlayableCtrl player, int level, AugmentationEventType eventType) : base(player, level, eventType)
 	{
-
+		enumerator = Bomb(true);
+		if (lvl < currentLevel)
+		{
+			CoroutineHandler.StopCoroutine(enumerator);
+			skillTime = 0f;
+			enumerator = Bomb(false);
+			CoroutineHandler.StartCoroutine(enumerator);
+			switch (lvl)
+			{
+				case 1:
+					skillTime = 240f;
+					break;
+				case 2:
+					skillTime = 210f;
+					break;
+				case 3:
+					skillTime = 180f;
+					break;
+				case 4:
+					skillTime = 150f;
+					break;
+				case 5:
+					skillTime = 120f;
+					break;
+			}
+			enumerator = Bomb(true);
+			CoroutineHandler.StartCoroutine(enumerator);
+		}
 	}
 
 	public override void AugmentationEffect(Entity sender, EventArgs e)
@@ -15,14 +47,18 @@ public class NuclearBomb : Augmentation
 			
 	}
 
-	public IEnumerator Bomb()
+	public IEnumerator Bomb(bool isInfinity)
 	{
-
-		yield return new WaitForSeconds(240f);
-
-		foreach (var enemy in UnityEngine.Object.FindObjectsOfType<EnemyCtrl>())
+		while (isInfinity)
 		{
-			enemy.TakeDamage(enemy.hp);
+			yield return new WaitForSeconds(skillTime);
+
+			foreach (var enemy in UnityEngine.Object.FindObjectsOfType<EnemyCtrl>())
+			{
+				//enemy.TakeDamage(enemy.hp);
+				enemy.GetComponent<Entity>().TakeDamage(player, enemy.hp);
+			}
 		}
 	}
+
 }
