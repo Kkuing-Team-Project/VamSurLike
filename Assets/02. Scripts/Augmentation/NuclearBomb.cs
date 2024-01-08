de42ef1368bfc3f2bb342d0e190ae2e6bdad502b
Fsahn <haider1315s@gmail.com>
2024-01-08 11:23:58 +0900

    .

---
+++ Assets/02. Scripts/Augmentation/NuclearBomb.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NuclearBomb : Augmentation
+{
+	public NuclearBomb(PlayableCtrl player, AugmentationEventType eventType) : base(player, eventType)
+	{
+
+	}
+
+	public override void AugmentationEffect(Entity sender, EventArgs e)
+	{
+		
+	}
+}
