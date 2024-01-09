914b5cffd8e2f03b42244c2c9383ec6619ab30a6
kyw04 <85551017+kyw04@users.noreply.github.com>
2024-01-10 01:39:41 +0900

    fix: spawner custom inspector

--- Assets/02. Scripts/Editor/SpawnerEditor.cs
+++ Assets/02. Scripts/Editor/SpawnerEditor.cs
@@ -1,19 +1,17 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditorInternal;
 
-[CustomEditor(typeof(Spawner)), CanEditMultipleObjects]
+[CustomEditor(typeof(Spawner))]
 public class SpawnerEditor : Editor
 {
     private SerializedProperty m_SpawnObjects;
-    private SerializedProperty m_prefab;
+    private SerializedProperty m_Waves;
 
     private void OnEnable()
     {
         m_SpawnObjects = serializedObject.FindProperty("spawnObjects");
-        m_prefab = serializedObject.FindProperty("prefab");
+        m_Waves = serializedObject.FindProperty("waves");
     }
 
     public override void OnInspectorGUI()
@@ -22,16 +20,32 @@ public class SpawnerEditor : Editor
         Spawner myTarget = (Spawner)target;
 
         myTarget.isWave = EditorGUILayout.Toggle("Is Wave", myTarget.isWave);
-        m_SpawnObjects = myTarget.isWave ? serializedObject.FindProperty("waves") : m_SpawnObjects = serializedObject.FindProperty("spawnObjects");
-        
-        EditorGUILayout.PropertyField(m_SpawnObjects.FindPropertyRelative("Array.size"), new GUIContent("List Count"));
-        EditorGUI.indentLevel = 1;
-        for (int i = 0; i < m_SpawnObjects.arraySize; ++i)
+        if (myTarget.isWave)
         {
-            EditorGUILayout.PropertyField(m_SpawnObjects.GetArrayElementAtIndex(i), new GUIContent(i.ToString()));
+            EditorGUILayout.PropertyField(m_Waves.FindPropertyRelative("Array.size"), new GUIContent("Wave Count"));
+            EditorGUI.indentLevel = 1;
+            for (int i = 0; i < m_Waves.arraySize; ++i)
+            {
+                GUILayout.BeginVertical("", new GUIStyle(GUI.skin.box));
+                EditorGUILayout.PropertyField(m_Waves.GetArrayElementAtIndex(i), new GUIContent("Wave " + i));
+                GUILayout.EndVertical();
+            }
+        }
+        else
+        {
+            EditorGUILayout.PropertyField(m_SpawnObjects.FindPropertyRelative("Array.size"), new GUIContent("Spawn Object Count"));
+            EditorGUI.indentLevel = 1;
+            for (int i = 0; i < m_SpawnObjects.arraySize; ++i)
+            {
+                GUI.color = Color.gray;
+                GUILayout.BeginVertical("", new GUIStyle(GUI.skin.box));
+                GUI.color = Color.white;
+                EditorGUILayout.PropertyField(m_SpawnObjects.GetArrayElementAtIndex(i), new GUIContent("Spawn Object " + i));
+                GUILayout.EndVertical();
+            }
         }
         EditorGUI.indentLevel = 0;
-        
+
         myTarget.isMax = EditorGUILayout.Toggle("Is Max", myTarget.isMax);
         if (myTarget.isMax)
         {
@@ -62,24 +76,15 @@ public class SpawnObjectDrawer : PropertyDrawer
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         label = EditorGUI.BeginProperty(position, label, property);
-
+        int startIndentLevel = EditorGUI.indentLevel;
         if (property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, label))
         {
-            // if (position.height > 16f)
-            // {
-            //     position.height = 16f;
-            //     EditorGUI.indentLevel += 1;
-            //     Rect contentPosition = EditorGUI.IndentedRect(position);
-            //     contentPosition.y = 18f;
-            // }
-
-            int indent = EditorGUI.indentLevel;
-            EditorGUI.indentLevel = 2;
+            EditorGUI.indentLevel = startIndentLevel + 1;
             EditorGUILayout.BeginVertical();
             EditorGUILayout.PropertyField(property.FindPropertyRelative("prefab"), new GUIContent("Prefab"));
             EditorGUILayout.PropertyField(property.FindPropertyRelative("type"), new GUIContent("Type"));
             EditorGUILayout.PropertyField(property.FindPropertyRelative("percent"), new GUIContent("Percent"));
-            EditorGUI.indentLevel = indent;
+            EditorGUI.indentLevel = startIndentLevel;
             EditorGUILayout.EndVertical();
         }
     }
@@ -91,14 +96,25 @@ public class WaveDrawer : PropertyDrawer
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         label = EditorGUI.BeginProperty(position, label, property);
-
+        SerializedProperty m_SpawnObjects = property.FindPropertyRelative("spawnObjects");
+        
         if (property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, label))
         {
             int indent = EditorGUI.indentLevel;
             EditorGUI.indentLevel = 2;
             EditorGUILayout.BeginVertical();
             EditorGUILayout.PropertyField(property.FindPropertyRelative("duration"), new GUIContent("Duration"));
-            EditorGUILayout.PropertyField(property.FindPropertyRelative("spawnObjects"), new GUIContent("Spawn Objects"));
+            // EditorGUILayout.PropertyField(property.FindPropertyRelative("spawnObjects"), new GUIContent("Spawn Objects"));
+            
+            EditorGUILayout.PropertyField(m_SpawnObjects.FindPropertyRelative("Array.size"), new GUIContent("Spawn Object Count"));
+            for (int j = 0; j < m_SpawnObjects.arraySize; ++j)
+            {
+                GUI.color = Color.gray;
+                GUILayout.BeginVertical("", new GUIStyle(GUI.skin.box));
+                GUI.color = Color.white;
+                EditorGUILayout.PropertyField(m_SpawnObjects.GetArrayElementAtIndex(j), new GUIContent("Spawn Object " + j));
+                GUILayout.EndVertical();
+            }
             EditorGUI.indentLevel = indent;
             EditorGUILayout.EndVertical();
         }
