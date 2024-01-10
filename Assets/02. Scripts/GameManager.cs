84a5ff14210ab842c664e249e7208904d424690f
superman2080 <superman2080@naver.com>
2024-01-10 20:02:34 +0900

    feat: GameManager

--- Assets/02. Scripts/GameManager.cs
+++ Assets/02. Scripts/GameManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     public GameObject ui;
+    public PlayableCtrl player;
 
 
     private void Awake()
@@ -17,6 +20,37 @@ public class GameManager : MonoBehaviour
 
         instance = this;
         DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        switch (scene.name)
+        {
+            case "Yeopseung":
+                ui.SetActive(true);
+                player = FindObjectOfType<PlayableCtrl>();
+                Debug.Log(player.gameObject.name);
+                break;
+            default:
+                ui.SetActive(false);
+                break;
+        }
+    }
+
+    public IEnumerator LoadAsyncScene(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
+    public void LoadInGame()
+    {
+        StartCoroutine(LoadAsyncScene("Yeopseung"));
+    }
 }
