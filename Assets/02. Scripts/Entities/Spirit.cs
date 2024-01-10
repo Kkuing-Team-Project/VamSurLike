d5ac330c94776879663f2cb4ddd5ed8fdf8998b9
superman2080 <superman2080@naver.com>
2024-01-10 16:17:51 +0900

    add player animation

---
+++ Assets/02. Scripts/Entities/Spirit.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SpiritState
+{
+
+}
+
+public class Spirit : Entity
+{
+    protected override void OnEntityDied()
+    {
+    }
+
+    protected override void OnTakeDamage(Entity caster, float dmg)
+    {
+    }
+
+    protected override void UpdateEntity()
+    {
+    }
+}
